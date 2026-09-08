109adbfc8259d2902c508f7932b3e8eaa704312f
Ahmed Hassan <150629643+ahmedhassan404@users.noreply.github.com>
2026-09-08 23:44:57 +0300

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+   public:
+    int maxProfit(vector<int>& prices) {
+        int l = 0;
+        int profit = 0;
+
+        for (int r = 1; r < prices.size(); r++) {
+            if (prices[r] < prices[l]) {
+                l = r;
+            } else {
+                profit = max(profit, prices[r] - prices[l]);
+            }
+        }
+        return profit ;
+    }
+};
